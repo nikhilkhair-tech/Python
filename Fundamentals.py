508924f286630d8fc7509e46e8c3faa72626f26f
nikhilkhair-tech <158139556+nikhilkhair-tech@users.noreply.github.com>
2025-02-19 14:39:44 +0530

    Created using Colab

---
+++ Fundamentals.py
@@ -0,0 +1,41 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "private_outputs": true,
+      "provenance": [],
+      "toc_visible": true,
+      "authorship_tag": "ABX9TyMCeH6lFwmTqGux/QE8BnrS"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "yeKE3RiOcXCf"
+      },
+      "outputs": [],
+      "source": [
+        "print(\"Tesjkt\")"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "e0oRowxAdUbH"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
